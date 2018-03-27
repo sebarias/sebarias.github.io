a70ba0f88adb47e81b05f25c396d78e49d675620
sabes <sabes.aries@gmail.com>
2018-03-27 08:30:03 -0300

    cambios

--- Tarea_4_3.js
+++ Tarea_4_3.js
@@ -1,12 +1,21 @@
 var t;
 var NUM_LINES;
+var contador;
+var cicle;
+var input;
+var output;
+
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
   background(20);
   t = 0;
-  NUM_LINES = 50;
+  NUM_LINES = 100;
   randomSeed(5);
+  contador = 0;
+  cicle = 0;
+
+  //noLoop();
 }
 
 function draw(){
@@ -15,35 +24,49 @@ function draw(){
   strokeWeight(0.5);
   translate(width / 2,height / 2);
   fill(150,0,0);
-  console.log(frameCount);
+  //console.log(frameCount);
 
   for(var i = 0; i < NUM_LINES; i++){
     //fill(random(150, 190));
+    var size = 1;
     stroke(255,150,360);
-    line(x1(t + i),y1(t + i),x2(t + i),y2(t + i));
+    line(x1(t + i),y1(t + i),x2(t + i) * size,y2(t + i) * size);
     stroke(0,150,360);
-    line(x1((t + i) * -1 ),y1((t + i) * -1),x2((t + i) * -1),y2((t + i) * -1));
+    line(x1((t + i) * -1 ),y1((t + i) * -1),x2((t + i) * -1) * size ,y2((t + i) * -1) * size);
+    if (cicle % 2 == 0 && cicle > 0){
+      rotate(HALF_PI);
+      stroke(255,150,360);
+      line(x1(t + i),y1(t + i),x2(t + i) * size,y2(t + i) * size);
+      stroke(0,150,360);
+      line(x1((t + i) * -1 ),y1((t + i) * -1),x2((t + i) * -1) * size,y2((t + i) * -1) * size);
+    }
+
   }
-  if (frameCount > 200){
+
+  if (contador > 200){
     t += 0.1; // velocidad del movimiento
   }
-  if (frameCount <= 200){
+  if (contador <= 200){
     t += 0.5;
   }
-  if (frameCount > 300){
+  if (contador > 300){
     t += 0.12;
   }
-  if (frameCount > 350){
+  if (contador > 350){
     t += 0.15;
   }
-  if (frameCount > 400){
+  if (contador > 400){
     t += 0.17;
   }
-  if (frameCount > 450){
+  if (contador > 450){
     t += 0.19;
   }
-
-
+  if(contador == 500){
+    contador = 0
+    cicle ++;
+  }else {
+    contador ++;
+  }
 
 }
 
@@ -63,3 +86,14 @@ function x2(t){
 function y2(t){
   return -cos(t / 20) * 200 + cos(t / 12) * 20;
 }
+
+WebMidi.enable(function (err) {
+
+  if (err) console.log("An error occurred", err);
+
+  WebMidi.inputs[0].addListener('pitchbend', "all", function(e) {
+    console.log("Pitch value: " + e.value);
+  });
+
+
+});
